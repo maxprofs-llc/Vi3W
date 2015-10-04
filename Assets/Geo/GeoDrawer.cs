a352eeeabb97b6a1923290104033bc9e3c80f168
QuantumLeap <joao.pequenao@cern.ch>
2015-10-04 10:34:07 +0200

    Added GeoDrawer class and object, to draw geographic limits on the globe.

---
+++ Assets/Geo/GeoDrawer.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Vectrosity;
+
+public class GeoDrawer : MonoBehaviour 
+{
+	private Material lineMaterial;
+	private List<VectorLine> lines;
+	private float lineThickness = 4f;
+	private float distanceToCenter = 12f;
+
+
+	void Start ()
+	{
+		lineMaterial = Resources.Load("DefaultLine3D") as Material;
+		lines = new List<VectorLine>();
+
+	}
+
+	public void SetGeoList(Dictionary<string, List<Vector2>> list)
+	{
+
+
+		foreach (KeyValuePair<string, List<Vector2>> point in list)
+		{			
+
+			List<Vector3> vertexes = new List<Vector3>();
+			List<Vector2> latlon =  (List<Vector2>)point.Value;
+
+			for (int i = 0; i < latlon.Count; i++)
+			{
+				Vector3 xyzpoint = XYZfromLatLon(latlon[i].x, latlon[i].y);
+				vertexes.Add(xyzpoint);
+			}			
+
+			VectorLine line = new VectorLine(point.Key, vertexes, lineMaterial.GetTexture(0), lineThickness, LineType.Continuous, Joins.Weld);
+        	line.material = lineMaterial;
+        	lines.Add(line);
+		}
+
+
+	}
+
+	public Vector3 XYZfromLatLon(float lat, float lon)
+	{
+		float latRad = Mathf.Deg2Rad * (lat);
+		float lonRad = Mathf.Deg2Rad * (lon);
+
+		float x = distanceToCenter * Mathf.Cos(latRad) * Mathf.Cos(lonRad);
+		float y = distanceToCenter * Mathf.Cos(latRad) * Mathf.Sin(lonRad);
+		float z = distanceToCenter * Mathf.Sin(latRad);
+
+		return new Vector3 (x,z,y);
+
+	}
+
+
+
+	void Update ()
+	{
+
+	}
+}
