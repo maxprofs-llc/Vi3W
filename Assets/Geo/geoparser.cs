4436719f6e8dcf72d859a6372bd3b2b60a49ae99
Nawar Halabi <nawar.halabi@gmail.com>
2015-10-04 09:30:11 +0100

    Fixed bugs in geoparser

--- Assets/Geo/geoparser.cs
+++ Assets/Geo/geoparser.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class GeoParser {
 	public Dictionary<string, List<Vector2>> getDistricts() {
-		districts = Resources.load("nepal-districts.geojson") as string;
+		string districts = (Resources.Load("nepal-districts.geojson") as TextAsset).text;
 
 		var data = new JSONObject(districts);
 		var res = new Dictionary<string, List<Vector2>>();
@@ -13,7 +13,7 @@ public class GeoParser {
 			string name = district["properties"]["DISTRICT"].str;
 			List<Vector2> arc = new List<Vector2>();
 			foreach(var point in district["properties"]["geometry"]["coordinates"][0].list) {
-				arc.Add(new Vector2(double.parse(point.list[0].str), double.parse(point.list[1].str)));
+				arc.Add(new Vector2(float.Parse(point.list[0].str), float.Parse(point.list[1].str)));
 			}
 			
 			res[name] = arc;
@@ -23,15 +23,15 @@ public class GeoParser {
 	}
 
 	public Dictionary<string, Vector2> getCities() {
-		cities = Resources.load("nepal-district-headquarters.geojson") as string;
+		string cities = (Resources.Load("nepal-district-headquarters.geojson") as TextAsset).text;
 		
 		var data = new JSONObject(cities);
 		var res = new Dictionary<string, Vector2>();
 		
 		foreach(var city in data["features"].list) {
 			string name = city["properties"]["HQ_NAME"].str;
-			double longtitude = double.parse(city["properties"]["geometry"]["coordinates"].list[0].str);
-			double latitude = double.parse(city["properties"]["geometry"]["coordinates"].list[1].str);
+			float longtitude = float.Parse(city["properties"]["geometry"]["coordinates"].list[0].str);
+			float latitude = float.Parse(city["properties"]["geometry"]["coordinates"].list[1].str);
 			Vector2 point = new Vector2(longtitude,latitude);
 			
 			res[name] = point;
